35f9c2d1bd12a30c1b6df011c8f4dce8b50ec9b4
Boris <9600929@gmail.com>
2022-11-02 19:27:28 +0400

    admin config1

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Validation\Rules\Exists;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -46,5 +47,9 @@ class User extends Authenticatable
     public function role()
     {
         return $this->belongsTo(Role::class);
+    }
+    public function isAdmin(): bool
+    {
+        return $this->role()->where('name', 'admin')->exists();
     }
 }
