a8ebb5aeb45fa26085d8c4a3438ab30490981faa
Boris <9600929@gmail.com>
2022-11-21 12:46:19 +0400

    create edit permission view

--- app/Http/Controllers/Admin/RoleController.php
+++ app/Http/Controllers/Admin/RoleController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Role;
-use Illuminate\Http\Request;
 
 class RoleController extends Controller
 {
@@ -13,5 +12,10 @@ class RoleController extends Controller
         $roles = Role::all();
 
         return view('admin.roles.index', compact('roles'));
+    }
+
+    public function create()
+    {
+        return view('admin.roles.create');
     }
 }
--- resources/views/admin/roles/create.blade.php
+++ resources/views/admin/roles/create.blade.php
@@ -1,12 +1,11 @@
 <x-admin-layout>
     <div class="mt-12 max-w-6xl mx-auto bg-slate-50 p-4 rounded">
         <div class="flex m-2 p-2">
-            <a href="{{ route('admin.permissions.index') }}"
-                class="px-4 py-2 bg-indigo-400 hover:bg-indigo-600 rounded">
+            <a href="{{ route('admin.roles.index') }}" class="px-4 py-2 bg-indigo-400 hover:bg-indigo-600 rounded">
                 Back</a>
         </div>
         <div class="max-w-md mx-auto bg-gray-100 p-6 mt-12 rounded">
-            <form class="space-y-5" method="POST" action="{{ route('admin.permissions.store') }}">
+            <form class="space-y-5" method="POST" action="{{ route('admin.roles.store') }}">
                 @csrf
                 <div>
                     <label for="name" class="text-xl">Name</label>
